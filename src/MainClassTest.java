35f9e0072d9f7010e29b24f12c2f4bc79069f4b9
geradot1337 <geradot1337@gmail.com>
2023-04-19 19:13:00 +0300

    Задание1

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,13 +1,16 @@
+import org.junit.Test;
+
 public class MainClassTest extends MainClass
 {
+    @Test
     public void testGetLocalNumber()
     {
         int b = this.getLocalNumber();
        if (b == 14) {
-           System.out.println("Успешно");
+           System.out.println("Метод getLoacalNumber выводит число 14");
        } else
        {
-           System.out.println("Не успешно");
+           System.out.println("Метод getLocalNumber не выводит число 14");
        }
     }
 }
