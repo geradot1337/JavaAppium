ee7d8f8135a299a55f50d66cd7ee81b5f43a1075
geradot1337 <geradot1337@gmail.com>
2023-04-19 19:34:38 +0300

    Задание2

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,9 +1,4 @@
 public class MainClass {
-private int class_number = 20;
-public int getClassNumber()
-{
-    return class_number;
 
-}
 }
 
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -2,15 +2,5 @@ import org.junit.Test;
 
 public class MainClassTest extends MainClass
 {
-    @Test
-    public void testGetLocalNumber()
-    {
-        int a = this.getClassNumber();
-       if (a>45)
-       {
-           System.out.println("Метод getClassNumber вовзращает больше 45");
-       } else {
-           System.out.println("Метод getClassNumber возвращает меньше 45");
-       }
-    }
+
 }
