66a22d2466e1875135cf3c3e9678f7788239d3cf
geradot1337 <geradot1337@gmail.com>
2023-04-19 22:33:21 +0300

    Тест 3

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,4 +1,10 @@
+import org.junit.Test;
+
 public class MainClass {
+private String class_string = "Hello, World";
+public String getClassString()
+{
+    return class_string;
 
 }
-
+}
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,6 +1,13 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest extends MainClass
 {
+@Test
+    public void testGetClassString()
+    {
 
+       Assert.assertTrue("Ошибка! Строка не содержит 'hello' или 'Hello'",this.getClassString().toLowerCase().contains("hello"));
+
+    }
 }
