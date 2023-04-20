703beee1f01067d2bdf8bee68e741dca566468c5
geradot1337 <geradot1337@gmail.com>
2023-04-20 12:14:44 +0300

    Тест 3

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -6,8 +6,8 @@ public class MainClassTest extends MainClass
 @Test
     public void testGetClassString()
     {
-
-       Assert.assertTrue("Ошибка! Строка не содержит 'hello' или 'Hello'",this.getClassString().toLowerCase().contains("hello"));
+        Assert.assertTrue("Ошибка! Строка не содержит 'Hello'",this.getClassString().contains("Hello"));
+        Assert.assertTrue("Ошибка! Строка не содержит 'hello'",this.getClassString().contains("hello"));
 
     }
 }
